78f7da7c799244b7fd3f074dfe506900bcbeb95e
kjalnes <kris.alnes@gmail.com>
2017-07-06 15:08:19 -0400

    ready for deploy

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -4,7 +4,9 @@ import { initCube, animate } from '../../utils/cubeFns.js';
 const App = () => {
     initCube();
     animate();
-    return (<div> Action! </div>)
-}
+    return (
+        <div> <a href='https://threejs.org/'>Three.js</a> WebGL cube - using CSS3DObject</div>
+    )
+};
 
 export default App;
